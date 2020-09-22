184ed751bf79825e6a1e66c48ad55a89e8a9255d
GondaKid <thienphuc16066@gmail.com>
2020-09-22 14:24:45 +0700

    change to has_many_through engagement

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,4 +1,6 @@
 class Subject < ApplicationRecord
-  has_and_belongs_to_many :students
-
+  has_many :enrollments
+  has_many :enrollments, :through => :enrollments
+  
 end
+  
---
+++ db/migrate/20200922063800_create_enrollments.rb
@@ -0,0 +1,7 @@
+# auto-generated migrate file when generate model enrollment
+class CreateEnrollments < ActiveRecord::Migration[6.0]
+  def change
+    add_column :enrollments, :created_at, :datetime, null: false
+    add_column :enrollments, :updated_at, :datetime, null: false
+  end
+end
