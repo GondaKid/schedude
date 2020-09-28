8355d00dba37d5f729ff598724835b8e90cb9ab3
Cable <vegitoson01@gmail.com>
2020-09-28 16:06:10 +0700

    Fix athgorithm for schedule create

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,18 +2,18 @@ class ApplicationController < ActionController::Base
   def any_overlapping_ranges(array_of_ranges)
     array_of_ranges.sort_by(&:first).each_cons(2).any?{|x,y|x.last>y.first}
   end
-    
+
   def schedule_on_days(data = [])
     on = ["T2", "T3", "T4", "T5", "T6", "T7"]
-    
-    # data = [{ :name_or_code, :on => , :time => range ..  }]
-    
-    days = []
+
+    # data = [{ :name_or_code, :on => , :time => range .. }]
+
     schedule_on_days = []
-    
+
     on.each do |on|
       sbj_on_day = []
-    
+      days = []
+
       #Get subject by day
       data.select{|x| x[:on].eql? on}.each do |sbj|
         sbj_on_day << sbj
@@ -26,36 +26,72 @@ class ApplicationController < ActionController::Base
     # List all subject times by day
     schedule_on_days
   end
-    
-  def schedule(schedule_on_days = [])
+
+  def schedule(schedule_all_days = [], next_steps = [], current = 0)
+    if schedule_all_days.blank?
+      return "ERROR"
+    end
+
     schedule = []
-    length = []
-    counter = [0, 0, 0, 0, 0, 0] # 6 Day
-    
-    counter.length.times {
-      length << schedule_on_days.length()
-    }
-    
-    total = length.inject{ |sum, e| sum *= e}
-    
-    (total).times{
-      days = []
-    
-      for index in 0..(counter.length - 1)
-      days << schedule_on_days[index][counter[index]]
+    lens = []
+
+    # If the first request
+    next_steps = [0, 0, 0, 0, 0, 0] if next_steps.blank?
+
+    for index in 0..(next_steps.length - 1)
+      lens << schedule_all_days[index].length
+    end
+
+    total = lens.inject{ |sum, e| sum *= e}.to_i
+
+    if current > total
+      return "MAX"
+    else
+      current = current + 1
+    end
+
+    days = []
+
+    for index in 0..(next_steps.length - 1)
+      days << schedule_all_days[index][next_steps[index]]
+    end
+
+    schedule << days
+
+    for index in (next_steps.length - 1).downto(0)
+      if next_steps[index] + 1 < lens[index] then
+        next_steps[index] += 1;
+        break;
       end
-    
-      schedule << days
-    
-      for index in (counter.length - 1).downto(0)
-        if counter[index] + 1 < length[index] then
-          counter[index] += 1;
-          break;
-        end
-        counter[index] = 0;
-      end
-    }
-    
-    schedule
+      next_steps[index] = 0;
+    end
+
+    return {:schedule_all_days => schedule_all_days, :schedule => schedule, :next_steps => next_steps, :current => current}
+  end
+
+  def parse_data_for_hcmus(data = [])
+    result = Array.new
+
+    data.split("\n").each do |line|
+      next if line.blank? || line.empty?
+
+      lines = line.split("\t")
+
+      times = if(!lines[7].is_a? Numeric)
+                lines[7].split(/[()]+/)
+              else
+                lines[8].split(/[()]+/)
+              end
+
+      sbj_info = Hash.new
+      sbj_info[:code] = lines[0]
+      sbj_info[:name] = lines[1]
+      sbj_info[:on] = times[0]
+      sbj_info[:time]= times[1].split("-").first.to_i..times[1].split("-").last.to_i
+
+      result << sbj_info
+    end
+
+    return result
   end
 end
