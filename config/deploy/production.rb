82b63461f2b5351237f24a7912570188ee1726b3
GondaKid <thienphuc16066@gmail.com>
2020-10-03 15:43:03 +0700

    change deploy branch to master

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,5 @@
 set :stage, :production
 set :rails_env, :production
 set :deploy_to, "/deploy/apps/schedude"
-set :branch, :config_deploy
+set :branch, "master"
 server "52.203.239.10", user: "deploy", roles: %w(web app db)
