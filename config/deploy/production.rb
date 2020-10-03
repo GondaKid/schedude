b98474088859e6ed072c1bdc6d91646fb69db773
GondaKid <thienphuc16066@gmail.com>
2020-10-03 15:33:47 +0700

    fix production file

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,4 +2,4 @@ set :stage, :production
 set :rails_env, :production
 set :deploy_to, "/deploy/apps/schedude"
 set :branch, :config_deploy
-server 52.203.239.10, user: "deploy", roles: %w(web app db)
+server "52.203.239.10", user: "deploy", roles: %w(web app db)
