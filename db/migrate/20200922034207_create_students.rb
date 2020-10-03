d05dc8a9b0f8f9605732752f33fd27946eb7d41c
GondaKid <thienphuc16066@gmail.com>
2020-10-03 17:40:32 +0700

    small fix

--- db/migrate/20200922034207_create_students.rb
+++ db/migrate/20200922034207_create_students.rb
@@ -2,7 +2,6 @@ class CreateStudents < ActiveRecord::Migration[6.0]
   def change
     create_table :students do |t|
       t.string :student_id
-      t.references :school, null: false, foreign_key: true
 
       t.timestamps
     end
