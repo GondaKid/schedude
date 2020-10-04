2d76c3b0a13fbb0ff4c3cca30a316d3a6d074fb9
GondaKid <thienphuc16066@gmail.com>
2020-10-04 18:57:25 +0700

    set up puma.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -29,7 +29,7 @@ set :puma_conf, -> {"#{shared_path}/puma.rb"}
 set :puma_access_log, -> {"#{shared_path}/log/puma_access.log"}
 set :puma_error_log, -> {"#{shared_path}/log/puma_error.log"}
 set :puma_role, :app
-set :puma_env, fetch(:rack_env, fetch(:rails_env, "staging"))
+set :puma_env, fetch(:rack_env, fetch(:rails_env, "production"))
 set :puma_threads, [0, 8]
 set :puma_workers, 0
 set :puma_worker_timeout, nil
