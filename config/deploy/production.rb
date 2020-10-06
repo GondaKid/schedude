0e2461a1113dc176ca00cf91c828ba9e9ccf0ecd
GondaKid <thienphuc16066@gmail.com>
2020-10-06 14:47:19 +0700

    change server ip in prodcution.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,4 +3,4 @@ set :rails_env, :production
 set :deploy_to, "/home/deploy/apps/schedude/"
 set :branch, "deploy"
 
-server "3.82.122.222", user: "deploy", roles: %w(web app db), primary: true
+server "schedude.me", user: "deploy", roles: %w(web app db), primary: true
