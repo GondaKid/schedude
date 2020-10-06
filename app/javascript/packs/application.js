04ddb661a917cf25dc649aadbd7cf160b889c557
GondaKid <thienphuc16066@gmail.com>
2020-10-06 16:52:30 +0700

    fix in application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,7 @@
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
-
-require("bootstrap")
-require("jquery")
+//= require("@rails/ujs").start()
+//= require("turbolinks").start()
+//= require("@rails/activestorage").start()
+//= require("channels")
+//= 
+//= require("bootstrap")
+//= require("jquery")
