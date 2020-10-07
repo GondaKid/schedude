6b1958ca4a9f191cb50425dda6c363728c38b944
Phong Phan <fongfan999@gmail.com>
2020-10-07 16:16:22 +0700

    Add Hello World to application.js file

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,9 @@
 //= require("turbolinks").start()
 //= require("@rails/activestorage").start()
 //= require("channels")
-//= 
+//=
 //= require("bootstrap")
 //= require("jquery")
+
+// Delete me when setting up server completely
+console.log('Hello World')
