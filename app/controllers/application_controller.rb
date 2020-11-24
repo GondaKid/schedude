11c5b59c7937bf28440b6a24e8e69a7366094741
CablePoolV4 <vegitoson01@gmail.com>
2020-11-24 12:05:53 +0700

    Handle when url not found

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+    def not_found
+        render plain: 'Oh! Trang này không có tồn tại!'
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'welcome/index'
   post '/students/:student_id/schedules/overview', to: 'schedules#overview', as: 'overview_student_schedule'
+  get '*unmatched_route', to: 'application#not_found'
 
   resources :students do
     resources :schedules
